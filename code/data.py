fix: normalization, delete slices

code/data.py
@@ -3,6 +3,7 @@ File to hold the data related functions
 
 """
 
+import string
 from torch.utils.data import Dataset, DataLoader
 import torch
 import numpy as np
@@ -46,6 +47,30 @@ def get_loaders(config):
 
     return train_loader, val_loader
 
+def normalize_values(f: string):
+    """
+    Function to normalize the data values between 0 and 1
+
+    Arguments:
+        f (string): filename of image file
+
+    """
+
+    source = sitk.GetArrayFromImage(sitk.ReadImage('source_resolution/' + f))
+    target = sitk.GetArrayFromImage(sitk.ReadImage('target_resolution/' + f))
+
+    if 'SET_A' in f:
+        source = source / 1936
+        target = target / 1936
+    if 'SET_B' in f:
+        source = source / 6207
+        target = target / 6207
+    if 'SET_C' in f:
+        source = source / 17274
+        target = target / 17274
+    
+    return source, target
+
 
 def read_files(config: dict) -> List[List]:
     """
@@ -72,9 +97,11 @@ def read_files(config: dict) -> List[List]:
 
     for i in range(len(data)):
         for f in sets[i]:
+            source, target = normalize_values(f)
+
             patient = {
-                "source": sitk.GetArrayFromImage(sitk.ReadImage('source_resolution/' + f)),
-                "target": sitk.GetArrayFromImage(sitk.ReadImage('target_resolution/' + f))
+                "source": source,
+                "target": target
             }
 
             data[i].append(patient)
@@ -162,7 +189,7 @@ def create_pairs(config: dict, data: List) -> (List[dict], List[dict]):
     return pairs_train, pairs_val
 
 
-def remove_empty_scans(config: dict, data: List, alpha: int = 200000) -> List[List]:
+def remove_empty_scans(config: dict, data: List, alpha: int = 25) -> List[List]:
     """
     Function to remove training and validation pairs that are empty
 

code/dip.py
@@ -27,7 +27,7 @@ def run(data_config):
     input_depth = 32
     img_shape = 1
     num_epochs = 10000 if torch.cuda.is_available() else 10
-    show_every = 500 if torch.cuda.is_available() else 1
+    show_every = 500 if torch.cuda.is_available() else 5
 
     # Set the random seed for reproducible results
     torch.manual_seed(0)
@@ -36,7 +36,8 @@ def run(data_config):
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
     for data_batch in iter(train_loader):
-        data_batch = train_loader.dataset.__getitem__(150) # Comment for complete run
+        # data_batch = train_loader.dataset.__getitem__(150) # Comment for complete run
+        
         
         # Define result dict
         results = {
@@ -51,9 +52,12 @@ def run(data_config):
         source = data_batch["source"][0]
         target = data_batch["target"][0]
 
+        source = np.array(source.squeeze(0))
+        target = np.array(target.squeeze(0))
+
         # Create masked image
         masked = np.zeros((256,192), dtype = float)
-        masked[::,1::2] = source / 255
+        masked[::,1::2] = source
         masked = np.expand_dims(masked, axis=0)
 
         # Create mask
@@ -63,7 +67,6 @@ def run(data_config):
         mask = np.expand_dims(mask, axis=0)
 
         # Prepare target
-        target = target / 255
         target = np.expand_dims(target, axis=0)
 
         # Create tensors